2c3c16fb84137be281825af458594b81d9a77d4e
Daniel Lemos <dls.2000@hotmail.com>
2021-05-05 14:26:56 -0300

    Listas / Hotel

---
+++ hotelExercicio/hotelExercicio/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace hotelExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Quarto[] quartos = new Quarto[10];
+
+            int n;
+
+            Console.WriteLine("Quantos quartos serão alugados?");
+            n = int.Parse(Console.ReadLine());
+
+        
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    string nome, email;
+                    int num;
+
+                    Console.WriteLine($"Aluguel numero {i + 1}");
+                    Console.WriteLine("Nome:");
+                    nome = Console.ReadLine();
+                    Console.WriteLine("Email:");
+                    email = Console.ReadLine();
+                    Console.WriteLine("Numero");
+                    num = int.Parse(Console.ReadLine());
+
+                if (quartos[num] == null || quartos[num].Ocupied == false)
+                {
+                    quartos[num] = new Quarto(nome, email, num);
+                }
+                else { Console.WriteLine("Quarto ocupado"); }
+                    
+                }
+
+                Console.WriteLine("Quartos ocupados:");
+
+                for (int i = 0; i < 10; i++)
+                {
+
+                   if(quartos[i] != null) Console.WriteLine(quartos[i].status());
+
+
+                }
+
+            }
+         
+
+        
+    }
+}
---
+++ hotelExercicio/hotelExercicio/Quarto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hotelExercicio
+{
+    class Quarto
+    {
+        public string Nome { get; private set;}
+        public string Email { get; private set;}
+        public bool Ocupied { get; private set;}
+        public int Number { get; private set;}
+
+        public Quarto(string nome, string email, int number) {
+
+                Nome = nome;
+                Email = email;
+                Number = number;
+            
+
+            // corrigir posteriormente com tratamento de exceções
+            
+            this.Ocupied = true;
+        }
+
+        /*
+        public void setQuarto(string nome, string email, int number) {
+
+            if (number > 10 || number < 1)
+            {
+                Console.WriteLine("Os números de quartos vão de 1 a 10");
+            }
+            else { Quarto(nome, email, number); }
+        }
+
+        */
+
+        public string status() {
+
+            if (Ocupied) { return $"{Number}: {Nome}, {Email}"; }
+            else { return "Quarto Vago"; }
+        
+        
+        }
+
+
+    }
+}
