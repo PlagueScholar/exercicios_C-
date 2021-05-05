a4efa494e528d359ba10bf2cc522b01cc662ae55
Daniel Lemos <dls.2000@hotmail.com>
2021-05-05 14:27:10 -0300

    a

---
+++ ExercicioListas/ExercicioListas/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioListas
+{
+    class Employee
+    {
+        public int Id { get; private set;}
+        public string Nome { get; private set; }
+        public double Salario { get; private set; }
+
+        public Employee(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void updateSalario(double prcnt){
+            if (prcnt <= 0) {
+                Console.WriteLine("Insira um valor maior que 0");
+            }
+            else{
+                Salario += Salario * (prcnt / 100);
+            }
+
+
+        }
+
+        public override string ToString()
+        {
+            return Id+","+ Nome + ", " + Salario;
+        }
+    }
+
+}
---
+++ ExercicioListas/ExercicioListas/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> lista = new List<Employee>();
+            int id_aux;
+            string nome_aux;
+            double salario_aux;
+
+
+
+            Console.WriteLine("How many employees will be registered");
+            int i = int.Parse(Console.ReadLine());
+
+            for (; i > 0; i--)
+            {
+                Console.WriteLine("Employee #" + i + ":");
+                Console.WriteLine("Id:");
+                id_aux = int.Parse(Console.ReadLine());
+                if (lista.Find(emp => emp.Id == id_aux) != null)
+                {
+                    Console.WriteLine("Id já utilizado, insira outro");
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Name:");
+                    nome_aux = Console.ReadLine();
+                    Console.WriteLine("Salary:");
+                    salario_aux = double.Parse(Console.ReadLine());
+                    lista.Add(new Employee(id_aux, nome_aux, salario_aux));
+                }
+            }
+
+            Console.WriteLine("Enter the id of the employee that will have a salary increase");
+          
+                        id_aux = int.Parse(Console.ReadLine());
+                        if (lista.Find(emp => emp.Id == id_aux) == null){
+                            Console.WriteLine("Non-Existing ID");
+                        }
+                        else {
+                            Console.WriteLine("Enter the percentage");
+                            double p = double.Parse(Console.ReadLine());
+                            lista.Find(emp => emp.Id == id_aux).updateSalario(p);
+                        }
+
+
+            foreach (Employee obj in lista)
+            {
+                Console.WriteLine(obj);
+
+            }
+
+
+
+        }
+    }
+}
